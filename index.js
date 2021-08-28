6ca747ce5d6b1eaf3076851cfc1da3bd0465d369
iveltondequeiroz <iveltondequeirozgmail.com>
2021-08-27 20:05:34 -0400

    create trade

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { ChainId, Fetcher, WETH, Route } = require('@uniswap/sdk');
+const { ChainId, Fetcher, WETH, Route, Trade, TokenAmount, TradeType } = require('@uniswap/sdk');
 const chainId = ChainId.MAINNET;
 const tokenAddress = '0x6b175474e89094c44da98b954eedeac495271d0f';
 
@@ -7,8 +7,11 @@ const init = async() => {
     const weth = WETH[chainId];
     const pair = await Fetcher.fetchPairData(dai, weth);
     const route = new Route([pair], weth);
-    console.log(route.midPrice.toSignificant(6));
-    console.log(route.midPrice.invert().toSignificant(6));
+    const trade = new Trade(route, new TokenAmount(weth, '100000000000000000'), TradeType.EXACT_INPUT);
+    console.log("DAI/WETH Mid Price", route.midPrice.toSignificant(6));
+    //console.log(route.midPrice.invert().toSignificant(6));
+    console.log("Execution Price: ", trade.executionPrice.toSignificant(6));
+    console.log("Next MidPrice: ", trade.nextMidPrice.toSignificant(6));
 }
 
 init();
